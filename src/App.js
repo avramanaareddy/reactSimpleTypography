ebf8fc2d8bac63f0b7ca9b7919f80e5ee0771e61
Vennkata <avramanaareddy@yahoo.com>
2020-11-04 20:41:59 -0500

    material ui examples

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ export default function App() {
     <div className="App">
       <h1>మెటీరియల్ యుఐ టైపోగ్రఫి </h1>
       <Typography variant="h4" color="primary">
-        ఆత్మ జ్ఞాని: <Typography variant="h5" color="secondary" component="span">
-        ఆత్మను గురించి తెలుసుకొంటాడు
-      </Typography>
+        ఆత్మ జ్ఞాని:{" "}
+        <Typography variant="h5" color="secondary" component="span">
+          ఆత్మను గురించి తెలుసుకొంటాడు
+        </Typography>
       </Typography>
       <br />
       <hr />
       <hr />
-      
       <br />
       మందము:{" "}
       <Button variant="contained" color="primary">
@@ -50,6 +50,34 @@ export default function App() {
       <Button variant="outlined" size="large" color="default">
         పెద్ద బొత్తాము
       </Button>
+      <br />
+      <br />
+      <Typography variant="h4" align="left" color="primary">
+        రాతను ఎడమపక్కకు పెట్టుట{" "}
+      </Typography>
+      <br />
+      <br />
+      <Typography variant="h4" align="center" color="secondary">
+        రాతను నడుమ పెట్టుట{" "}
+      </Typography>
+      <br />
+      <br />
+      <Typography variant="h4" align="right" color="default">
+        రాతను కుడి పక్కకు పెట్టుట{" "}
+      </Typography>
+      <Typography color="primary" wrap>
+        మన అమ్మనుడి అయిన తెలుగు గొప్పదనం ఎంత చెప్పినా తక్కువే. తెలుగులోని మాటలు
+        ఎంతో యెఱుక[wisdom ] తో కూడుకొని ఉన్నాయి అని చెప్పుకోవచ్చు. ఎల్ల[every ]
+        తెలుగు మాటలను లోతుగా చూస్తే ఎంత యెఱుక అందులో దాగి ఉన్నదో తెలియును.
+        మచ్చుకు ఈ మాటలను తీసుకొని చూద్దాము. ఎల్లిగాడు, మల్లిగాడు, అనే మాటలను
+        విడదీస్తే ఎల్లి+గ +ఆడు=> ఎల్లిగా ఆడేవాడు. మల్లి+గ+ ఆడు=> మల్లిగా
+        ఆడేవాడు. ఇక్కడ ఎవరు ఆడుతున్నది ఎల్లిగా అని అంటే ఎల్లిగాని ఒంటినిండా
+        పైనుండి కిందికి సత్తుగా పరచబడి రవ్వ రవ్వలోను కదలిక సత్తుగా ఉండి మన
+        ఒంటిని ఆడిస్తున్నది ఎదరి[ఆత్మ] . అందుకే ఆ మాట వింటే మనకు ఏమి
+        గుర్తురావాలి అంటే ఎల్లిగాని గా ఎల్లిగాని ఒంటిని అడిస్తున్నాడు అని
+        తెల్లం[understand ] అవుతున్నది. కాబట్టి ఎల్లిగాడు అను మాట యెఱుక తో కూడి
+        ఉన్న మాట.
+      </Typography>
     </div>
   );
 }
